198b3bbc84b4d354ea8b574f8d80b69f09e0cf40
dylan <dibenedittod@gmail.com>
2026-02-10 23:04:12 -0500

    working make file and file reading

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char* argv[]) {
     Scan scan;
-    if(argc > 1) {
+    if(argc > 2) {
         std::cout << "Usage: ditto [filename]" << std::endl;
         exit(64);
-    } else if (argc == 1) {
-        std::string path(argv[0]);
+    } else if (argc == 2) {
+        std::string path(argv[1]);
         scan.runFile(path);
     } else {
         // run prompt
--- src/scan/scan.cpp
+++ src/scan/scan.cpp
@@ -1,38 +1,52 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <vector>
+#include "../tokens/token.h"
+#include "./scan.h"
 
-class Scan {
-public:
-    void runFile(const std::string& file) {
-        // Open the file in binary mode for verbatim reading
-        std::ifstream f(file, std::ios_base::binary | std::ios_base::in);
-        
-        if (!f.is_open()) { // check if file opened
-            throw std::runtime_error("Failed to open " + file);
-        }
-
-        // Use iterators to read the entire file content into the string
-        std::string source((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
-
-        if (!f && !f.eof()) { // check for read errors
-             throw std::runtime_error("Failed to read " + file);
-        }
-
-        Scan::run(source);
+void Scan::runFile(const std::string& file) {
+    // Open the file in binary mode for verbatim reading
+    std::ifstream f(file, std::ios_base::binary | std::ios_base::in);
+    
+    if (!f.is_open()) { // check if file opened
+        throw std::runtime_error("Failed to open " + file);
     }
 
-    void runPrompt() {
-        while(true) {
-            std::string line;
-            std::cout << "> ";
-            std::cin >> line;
+    // Use iterators to read the entire file content into the string
+    std::string source((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
 
-            Scan::run(line);
-        }
+    if (!f && !f.eof()) { // check for read errors
+         throw std::runtime_error("Failed to read " + file);
     }
 
-    void run(const std::string source) {
+    run(source);
+}
 
+void Scan::runPrompt() {
+    while(true) {
+        std::string line;
+        std::cout << "> ";
+        std::getline(std::cin, line);
+
+        run(line);
     }
-};
+}
+
+void Scan::run(const std::string& source) {
+    std::cout<<source<<std::endl;
+    // Tokenizer tokenizer(source);
+    // std::vector<Token> tokens = tokenizer.scanTokens();
+    //
+    // for(Token t : tokens) {
+    //     std::cout<<t<<std::endl;
+    // }
+}
+
+void Scan::error(const int line, const std::string& message) {
+    report(line, "", message);
+}
+
+void Scan::report(const int line, const std::string& where, const std::string& message) {
+    std::cerr << "[line " << line << "] Error " << where << ": " << message;
+}
